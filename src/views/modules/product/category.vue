4b006643f340295f242ae93fa29342c7bd092a0b
imgyh <1974355683@qq.com>
2023-02-18 21:27:18 +0800

    feat(product): 初始化项目,商品分类显示
    从renren-fast-vue v1.2.2下载并初始化, 完成商品分类显示功能

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,35 @@
+<template>
+    <el-tree :data="category" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            category: [],
+            defaultProps: {
+                children: 'children',
+                label: 'name'
+            }
+        };
+    },
+    methods: {
+        handleNodeClick(data) {
+            console.log(data);
+        },
+        getCategoryTree() {
+            this.$http({
+                url: this.$http.adornUrl('/product/category/list/tree'),
+                method: 'get',
+            }).then(({ data }) => {
+                this.category = data.data
+                console.log(data.data)
+            })
+        }
+    },
+    created () {
+      this.getCategoryTree()
+    }
+};
+</script>
