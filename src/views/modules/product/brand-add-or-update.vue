8b864eccb8032d03634b4ffb44bd8000a9d959ba
imgyh <1974355683@qq.com>
2023-02-26 18:41:24 +0800

    feat(brand): 修改状态更新接口

--- src/views/modules/product/brand-add-or-update.vue
+++ src/views/modules/product/brand-add-or-update.vue
@@ -58,7 +58,7 @@ export default {
         descript: "",
         showStatus: 1,
         firstLetter: "",
-        sort: 0
+        sort: ""
       },
       dataRule: {
         name: [{ required: true, message: "品牌名不能为空", trigger: "blur" }],
@@ -92,7 +92,7 @@ export default {
         sort: [
           {
             validator: (rule, value, callback) => {
-              if (value == "") {
+              if (value === "") {
                 callback(new Error("排序字段必须填写"));
               } else if (!Number.isInteger(value) || value<0) {
                 callback(new Error("排序必须是一个大于等于0的整数"));
